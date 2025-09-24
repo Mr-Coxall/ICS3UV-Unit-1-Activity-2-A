29571ff136bbfa79023e3d69813f9bf66feb4bf0
Patrick Coxall <mr.coxall@mths.ca>
2025-09-24 00:21:04 +0000

    update

---
+++ main.go
@@ -0,0 +1,33 @@
+/*
+ * Author Mr Coxall
+ * Version 1.0.0
+ * Date 2025-01-01
+ * This is the ...
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize an integer
+	var count int = 0;
+
+	// Print it out
+	fmt.Println("The number is:", count)
+
+	// declaring a string then initializing its value
+	var grade11CourseCode string; // declaring
+	grade11CourseCode = "ICS3UV"; // initializing
+
+	// declaring a string and initializing its value
+	var grade12CourseCode string = "ICS3UV";
+
+
+
+
+
+
+	fmt.Println("The number is:", grade11CourseCode)
+	fmt.Println("The number is:", grade12CourseCode)
+}
---
+++ main2.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Mr Coxall
+ * @version 1.0.0
+ * @date 2025-01-01
+ * @fileoverview This program show variable declarations.
+ */
+
+// number data type count
+let count: number = 20;
+
+// number data type big_number
+let big_number: number = 14500000000;
+
+// number data type temperature
+let temperature: number = -32.4;
+
+// number data type molecules
+let molecules: number = 158799625.35;
+
+// string data type
+let courseCode: string = "ICS3U";
+
+// boolean data type play_again
+let play_again: boolean = true;
+
+// display each data type to the screen
+console.log("Can you count up to " + count + "?");
+console.log("This is a really big number " + big_number);
+console.log("The outside temperature currently is " + temperature);
+console.log("A drop of water might have " + molecules + " molecules");
+console.log("Do you like " + courseCode + "?");
+console.log("True/False Computer programming is cool: " + play_again);
